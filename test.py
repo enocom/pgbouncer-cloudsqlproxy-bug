d81234a486d0fc68ca3dc2d07ca51b7464e41616
Eno Compton <enocom@google.com>
2024-04-20 16:29:51 -0600

    Use more conistent comparision
    This changes the comparison to be:
    - pgbouncer + proxy to Cloud SQL
    - pgbouncer + direct connection to Cloud SQL
    - direct connection to Cloud SQL

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from time import time
-from typing import Any
 
 import anyio
 import psycopg
@@ -10,23 +9,24 @@ import psycopg
 async def main() -> None:
     async def hog(dsn: str, durations: list[float]):
         start = time()
-        conn = await psycopg.AsyncConnection[Any].connect(dsn)
-        await conn.execute('SELECT pg_sleep(10)')
+        async with await psycopg.AsyncConnection.connect(dsn) as aconn:
+            await aconn.execute('SELECT pg_sleep(10)')
         end = time()
         durations.append(end - start)
 
     for dsn in (
-        # connect to cloudsql via pgbouncer
-        'postgres://postgres:postgres@127.0.0.1:5432/cloudsql',
-        # connect to postgres via pgbouncer
-        'postgres://postgres:postgres@127.0.0.1:5432/local',
-        # connect to cloudsql directly
-        'postgres://postgres:postgres@127.0.0.1:5434/postgres',
+        # connect to cloudsql via pgbouncer + proxy
+        'postgres://postgres:postgres@127.0.0.1:5432/proxy',
+        # connect to cloudsql via pgbouncer + direct
+        'postgres://postgres:postgres@127.0.0.1:5432/direct',
+        # connect to cloudsql direct (change IP to be private or public iP)
+        'postgres://postgres:postgres@35.238.106.212/postgres',
     ):
         durations: list[float] = []
         async with anyio.create_task_group() as tg:
             for _ in range(32):
                 tg.start_soon(hog, dsn, durations)
-        print(f'{dsn}:\n{[f'{d:.2f}s' for d in durations]}\n\n')
+        x = [f'{d:.2f}s' for d in durations]
+        print(f'{dsn}:\n{x}\n\n')
 
 anyio.run(main)
